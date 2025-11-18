8dcc3b64dd52364db3dd8604b1a2eed4382813c0
t06k <kumanozi13@yahoo.co.jp>
2025-11-18 18:36:55 +0900

    更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -220,7 +220,7 @@ export default function Home() {
 
         <footer className="mt-8 text-center text-sm text-gray-600">
           <p>
-            データ元:{' '}
+            データ:{' '}
             <a
               href="https://universalis.app/"
               target="_blank"
