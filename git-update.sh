dfb4a8706a23fc123118b6bfc6038742f7e54832
t06k <kumanozi13@yahoo.co.jp>
2025-11-20 18:19:29 +0900

    更新

--- git-update.sh
+++ git-update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# 引数でメッセージを渡せるようにする
+# 全ファイルをステージングして "更新" でコミット＆push
+git branch
 git add -A
-git commit -m "$1"
-git push origin HEAD
+git commit -m "更新"
+git push origin main
+
+# #!/bin/bash
+# # 引数でメッセージを渡せるようにする
+# git add -A
+# git commit -m "$1"
+# git push origin HEAD
